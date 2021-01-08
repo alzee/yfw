712390a3de35265faee62003d43fbf4695e4b8d2
Al Zee <z@alz.ee>
2021-01-08 15:31:36 +0800

    add priceMin2 field

--- alter_table.sql
+++ alter_table.sql
@@ -1,2 +1,3 @@
-alter table drug add sn varchar(50) default null after drugId;
-alter table drug add approvalNum varchar(50) default null after sn;
+alter table drug add if not exists sn varchar(50) default null after drugId;
+alter table drug add if not exists approvalNum varchar(50) default null after sn;
+alter table drug add if not exists priceMin2 varchar(50) default null after priceMin;
