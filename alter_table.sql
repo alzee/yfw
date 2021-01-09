69b71fff3ac66adc5e911f1a6023a428e69e1382
Al Zee <z@alz.ee>
2021-01-09 10:48:33 +0800

    add updateOn field

--- alter_table.sql
+++ alter_table.sql
@@ -1,3 +1,4 @@
 alter table drug add if not exists sn varchar(50) default null after drugId;
 alter table drug add if not exists approvalNum varchar(50) default null after sn;
 alter table drug add if not exists priceMin2 varchar(50) default null after priceMin;
+alter table drug add if not exists updateOn timestamp not null default current_timestamp on update current_timestamp
