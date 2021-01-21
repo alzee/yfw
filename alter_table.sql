d72d7f411bebe52d16a149eab48cb0b1a737d654
Al Zee <z@alz.ee>
2021-01-21 12:58:37 +0800

    add fields: ourStock stockPriceMin stockPriceMin2

--- alter_table.sql
+++ alter_table.sql
@@ -1,4 +1,7 @@
 alter table drug add if not exists sn varchar(50) default null after drugId;
 alter table drug add if not exists approvalNum varchar(50) default null after sn;
 alter table drug add if not exists priceMin2 varchar(50) default null after priceMin;
-alter table drug add if not exists updateOn timestamp not null default current_timestamp on update current_timestamp
+alter table drug add if not exists updateOn timestamp not null default current_timestamp on update current_timestamp;
+alter table drug add if not exists ourStock varchar(50) default null after ourPrice;
+alter table drug add if not exists stockPriceMin varchar(50) default null after priceMin;
+alter table drug add if not exists stockPriceMin2 varchar(50) default null after priceMin2;
